6f6f6b753f1779e26d0dee5dbee502f6c48db506
Aarush <pruthi199@gmail.com>
2020-11-03 09:13:24 +0530

    app.js

---
+++ app.js
@@ -0,0 +1,120 @@
+//Elements
+const clear = document.querySelector(".clear");
+const dateElement = document.getElementById("date") ;
+const input = document.getElementById('item');
+const list = document.getElementById('list');
+
+//ClassNames
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle-thin";
+const LINE_THROUGH = "lineThrough";
+
+
+//VARIABLES
+let LIST = []
+, id = 0;
+
+ let data = localStorage.getItem("TODO");
+
+
+if(data){
+  LIST = JSON.parse(data);
+  id = LIST.length;
+  loadList(LIST);
+}else{
+  List = [];
+  id = 0;
+}
+
+function loadList(array){
+  array.forEach(function(item){
+    addTodo(item.name, item.id, item.done, item.trash)
+  });
+}
+  
+  clear.addEventListener("click",function(){
+    localStorage.clear();
+    location.reload();
+  });
+
+
+//Date
+
+const options = {weekday : "long", month : "short" , day : "numeric"};
+const today = new Date();
+dateElement.innerHTML = today.toLocaleDateString("en-US" , options );
+
+
+//FUNCTION
+
+function addTodo(toDo,id,done,trash) {
+  if(trash) {return;}
+
+  const DONE = done ? CHECK : UNCHECK ;
+  const LINE = done ? LINE_THROUGH : "" ;
+
+    
+   const item = `
+   <li class = "item">
+                 <i class="fa ${DONE} co" job ="complete" id="${id}"></i>
+                 <p class = "text ${LINE}">${toDo}</p>
+                 <i class="fa fa-trash-o de" job ="delete" id="${id}"></i>
+                 </li>
+   `;
+    
+   const position = "beforeend"
+
+          list.insertAdjacentHTML(position,item)
+                   }
+
+                  
+  document.addEventListener("keyup",function(event){
+      if(event.keyCode === 13){
+       const toDo = input.value;
+
+      if(toDo) {
+            addTodo(toDo,id,false,false);
+
+            LIST.push({
+                  name : toDo,
+                  id : id,
+                  done : false,
+                  trash : false,
+                              
+            });
+             localStorage.setItem("TODO",JSON.stringify(LIST));
+        id++;
+      }
+          input.value ="";
+         
+      }
+  });
+
+//addTodo("drink",1,true,false);
+
+function completeTodo (element) {
+  element.classList.toggle(CHECK);
+  element.classList.toggle(UNCHECK);
+  element.parentNode.querySelector(".text").classList.toggle(LINE_THROUGH);
+
+  LIST[element.id].done = LIST[element.id].done ? false :true;
+}  
+function removeTodo (element) {
+  element.parentNode.parentNode.removeChild(element.parentNode);
+
+  LIST[element.id].trash = true;
+}
+
+
+list.addEventListener("click",function(event) {
+  const element = event.target; 
+  const elementJob = element.attributes.job.value;
+
+  if(elementJob == "complete"){
+     completeTodo(element);
+  } else if (elementJob == "delete"){
+     removeTodo(element);
+  }
+
+     localStorage.setItem("TODO",JSON.stringify(LIST));
+});
